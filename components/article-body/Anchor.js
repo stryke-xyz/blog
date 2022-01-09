2435b44ef3cdbfc534300978d14f3fc3c0a8f1fa
hibou-de-nuit <hasminosmin2@gmail.com>
2022-01-09 19:44:48 +0400

    fix: anchor tag overflow

--- components/article-body/Anchor.js
+++ components/article-body/Anchor.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Anchor = ({ children, href }) => {
   return (
-    <a className="text-blue-500 underline cursor-pointer" href={href}>
+    <a className="text-blue-500 underline cursor-pointer break-all" href={href}>
       {children}
     </a>
   )
