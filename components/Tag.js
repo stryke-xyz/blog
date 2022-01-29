91ba9491edc361df5b2c3ef486eaa2424c19d727
HolyPepperone <85767768+hibou-de-nuit@users.noreply.github.com>
2022-01-29 16:58:41 +0400

    feat: add infinite scroll (#13)
    quick search tags in homepage

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-2 text-sm font-medium uppercase text-primary dark:text-wave-blue">
+      <a className="mr-2 text-sm font-medium uppercase text-primary dark:text-wave-blue hover:underline">
         {text.split(' ').join('-')}
       </a>
     </Link>
