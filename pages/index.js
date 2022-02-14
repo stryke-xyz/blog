af32eecbe195d700051898b8d0e3a023b3a73c35
hibou-de-nuit <hasminosmin2@gmail.com>
2022-02-15 00:41:13 +0400

    fix: untranslated text

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ export default function Home({ stories }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            {siteMetadata.pageTitle[selectedLanguage]}
           </h1>
           <p className="text-lg leading-7 text-stieglitz dark:text-gray-400">
             {siteMetadata.description[selectedLanguage]}
