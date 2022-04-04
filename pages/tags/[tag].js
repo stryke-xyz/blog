ffd4f136eef6fb65308aff2364e3997b9e25c043
hibou-de-nuit <hasminosmin2@gmail.com>
2022-04-04 14:07:30 +0400

    fix: tag filtered articles for zh

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+
 import { TagSEO } from '@/components/SEO'
 
 import ListLayout from '@/layouts/ListLayout'
+
+import { LocalizationContext } from 'contexts/Localization'
 
 import kebabCase from '@/lib/utils/kebabCase'
 import Storyblok from '@/lib/utils/storyblok-service'
@@ -21,20 +25,39 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const { data } = await Storyblok.get(`cdn/stories/`, {
+  let data = await Storyblok.get(`cdn/stories/`, {
     starts_with: 'articles/',
     per_page: 100,
   })
 
+  let zh_data = await Storyblok.get(`cdn/stories/`, {
+    starts_with: 'zh/articles/',
+    per_page: 100,
+  })
+
   // Load filtered posted based on tags
-  const filteredPosts = data.stories.filter((post) =>
+  const filteredPosts = data.data.stories.filter((post) =>
     post.tag_list.map((t) => kebabCase(t)).includes(params.tag)
   )
 
-  return { props: { posts: filteredPosts, tag: params.tag } }
+  const filteredPostsZh = zh_data.data.stories.filter((post) =>
+    post.tag_list.map((t) => kebabCase(t)).includes(params.tag)
+  )
+
+  return {
+    props: {
+      posts: {
+        en: filteredPosts,
+        zh: filteredPostsZh,
+      },
+      tag: params.tag,
+    },
+  }
 }
 
 export default function Tag({ posts, tag }) {
+  const { selectedLanguage } = useContext(LocalizationContext)
+
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1).toUpperCase()
   return (
     <>
@@ -42,7 +65,7 @@ export default function Tag({ posts, tag }) {
         title={`${tag} - ${siteMetadata.author}`}
         description={`${tag} tags - ${siteMetadata.author}`}
       />
-      <ListLayout posts={posts} title={title} />
+      <ListLayout posts={posts[selectedLanguage]} title={title} />
     </>
   )
 }
