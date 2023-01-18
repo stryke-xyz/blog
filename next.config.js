e452a2544ee417e4c8e0a7e067ee1826c15941aa
WitherBlock <witherblock@protonmail.com>
2023-01-18 16:06:01 +0400

    chore: remove spg timeout

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
-  staticPageGenerationTimeout: 1200,
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx'],
   eslint: {
